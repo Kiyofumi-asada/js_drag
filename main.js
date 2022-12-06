ab5e83b445bdd77da3bb6d36e5e69aad2b6726d4
k.a <kiyofumi.a.0122+git@gmail.com>
2022-12-06 13:12:58 +0900

    modify

--- main.js
+++ main.js
@@ -1,21 +1,46 @@
-let parentElm = document.querySelector('.drag-wrap');
-let ulElm = document.createElement('ul');
-let aElm = document.createElement('a');
-aElm.id = 'link-to-top';
-aElm.href = '/';
-aElm.textContent = 'top';
+let print = (v) => process.stdout.write(String(v));
 
-parentElm.appendChild(ulElm);
-parentElm.appendChild(aElm);
-for (let i = 0; i <= 5; i++) {
-  let liElm = document.createElement('li');
-  liElm.textContent = i;
-  ulElm.appendChild(liElm);
+month = [];
+week = ['  日', ' 月', ' 火', ' 水', ' 木', ' 金', ' 土', '\n'];
+day = [];
+
+let date = new Date();
+let y = date.getFullYear(); // 年
+let m = date.getMonth() + 1; // 月
+let d = date.getDate(); // 日
+let h = date.getHours(); // 時
+let dayOfWeek = date.getDay(); // 曜日(数値)
+console.log(dayOfWeek);
+
+for (let i = 1; i <= 12; i++) {
+  month.push(i);
 }
 
-let add =
-  '<h3>HTMLをソースコードで追加する</h3><p id="text1" class="normal_text">テキスト1</p><p id="text2" class="normal_text">テキスト2</p>';
+for (let i = 1; i <= 31; i++) {
+  if (i % 7 == 0 && i != 0) {
+    i = String(i);
+    if (i.length == 1) {
+      day.push('  ' + i + '\n');
+    } else {
+      day.push(' ' + i + '\n');
+    }
+  } else {
+    i = String(i);
+    if (i.length == 1) {
+      day.push('  ' + i);
+    } else {
+      day.push(' ' + i);
+    }
+  }
+}
 
-parentElm.innerHTML = add;
-
-parentElm.insertAdjacentHTML('afterbegin', add);
+month.forEach((m) => {
+  print('\n');
+  print('\n ======== ' + m + '月 ' + '======== \n');
+  week.forEach((v) => {
+    print(v);
+  });
+  day.forEach((v) => {
+    print(v);
+  });
+});
